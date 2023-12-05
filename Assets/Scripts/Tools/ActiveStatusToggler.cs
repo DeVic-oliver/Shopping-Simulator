b7f92a7671043923bfdad057f5133e694c8d1149
Victor Matheus <matheuslima.o183@gmail.com>
2023-12-05 14:11:38 -0300

    Basic tool to activate or deactivate a component

---
+++ Assets/Scripts/Tools/ActiveStatusToggler.cs
@@ -0,0 +1,22 @@
+﻿namespace Assets.Scripts.Tools
+{
+    using UnityEngine;
+    
+    public class ActiveStatusToggler : MonoBehaviour
+    {
+        [SerializeField] private KeyCode _actionKey;
+        [SerializeField] private GameObject _gameObjectToToggle;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(_actionKey))
+                ToggleGameObjectActiveStatus();
+        }
+
+        private void ToggleGameObjectActiveStatus()
+        {
+            bool newActiveStatus = !_gameObjectToToggle.activeSelf;
+            _gameObjectToToggle.SetActive(newActiveStatus);
+        }
+    }
+}
