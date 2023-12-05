ab0e5c4e26eb7aba190ae07219bbd8912391056e
Victor Matheus <matheuslima.o183@gmail.com>
2023-12-05 18:18:25 -0300

    Integrates the player item and animators

---
+++ Assets/Scripts/Player/PlayerItemEquipGateway.cs
@@ -0,0 +1,63 @@
+﻿namespace Assets.Scripts.Player
+{
+    using Assets.Scripts.Core.Enums;
+    using Assets.Scripts.ScriptableObjects;
+    using UnityEngine;
+    
+    public class PlayerItemEquipGateway : MonoBehaviour
+    {
+        [Header("Head params")]
+        [SerializeField] private GameObject _head;
+        [SerializeField] private Animator _headController;
+        
+        [Header("Outfit params")]
+        [SerializeField] private GameObject _outfit;
+        [SerializeField] private Animator _outfitController;
+
+
+        public void EquipItem(SCO_Item ItemData)
+        {
+            if (IsItemHeadType(ItemData))
+            {
+                _headController.runtimeAnimatorController = ItemData.HeadAnimatorController;
+                _head.SetActive(true);
+            }
+            else
+            {
+                _outfitController.runtimeAnimatorController = ItemData.OutfitAnimatorController;
+                _outfit.SetActive(true);
+            }
+        }
+
+        public void UnequipItem(SCO_Item ItemData)
+        {
+            if (IsItemHeadType(ItemData))
+            {
+                _headController.runtimeAnimatorController = null;
+                _head.SetActive(false);
+            }
+            else
+            {
+                _outfitController = null;
+                _outfit.SetActive(false);
+            }
+        }
+
+        private bool IsItemHeadType(SCO_Item ItemData)
+        {
+            return (ItemData.Type == ItemType.Head);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
