6bd20aa383b4174b4c32b5d71e56c4b5db86fde6
Victor Matheus <matheuslima.o183@gmail.com>
2023-12-05 18:18:01 -0300

    Inherits the item class and equips in the player

---
+++ Assets/Scripts/Player/PlayerBagItem.cs
@@ -0,0 +1,38 @@
+﻿namespace Assets.Scripts.Player
+{
+    using Assets.Scripts.Core.Item;
+    using UnityEditor.Animations;
+    using UnityEngine;
+    
+    public class PlayerBagItem : Item
+    {
+        [SerializeField] private PlayerItemEquipGateway _equipGateway;
+      
+        private bool _isEquipped;
+
+
+        public void ToggleEquipment()
+        {
+            if(!_isEquipped) 
+            { 
+                _isEquipped = true;
+                _equipGateway.EquipItem(ItemData);
+            }
+            else
+            {
+                _isEquipped = false;
+                _equipGateway.UnequipItem(ItemData);
+            }
+        }
+
+        void Start()
+        {
+            SetPrice();
+        }
+
+        protected override void SetPrice()
+        {
+            _itemPriceTMP.text = GetNumberString(ItemData.ShopkeeperBuyPrice);
+        }
+    }
+}
