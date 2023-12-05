697302e2c741e9de0235386755a82398cc353eae
Victor Matheus <matheuslima.o183@gmail.com>
2023-12-05 14:12:18 -0300

    Integrates player movement with movement animations

---
+++ Assets/Scripts/Player/PlayerMovementAnimator.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets.Scripts.Player
+{
+    using UnityEngine;
+    
+    public class PlayerMovementAnimator : MonoBehaviour
+    {
+        [SerializeField] private PlayerMovement _movement;
+        [SerializeField] private Animator _animator;
+
+
+        void Update()
+        {
+            _animator.SetBool("isMoving", _movement.IsMoving);
+            _animator.SetFloat("moveX", _movement.HorizontalAxisValue);
+            _animator.SetFloat("moveY", _movement.VerticalAxisValue);
+        }
+    }
+}
