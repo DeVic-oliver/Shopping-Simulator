6a7d23da71bfe8f13360f6d8d36e248b54afbf50
Victor Matheus <matheuslima.o183@gmail.com>
2023-12-05 18:19:01 -0300

    Inherits the Item class and set the sell price TMP

---
+++ Assets/Scripts/Shopkeeper/ShopkeeperItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets.Scripts.Shopkeeper
+{
+    using Assets.Scripts.Core.Item;
+    
+    public class ShopkeeperItem : Item
+    {
+        void Start()
+        {
+            SetPrice();
+        }
+
+        protected override void SetPrice()
+        {
+            _itemPriceTMP.text = GetNumberString(ItemData.ShopkeeperSellPrice);
+        }
+
+    }
+}
