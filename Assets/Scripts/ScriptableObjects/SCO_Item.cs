121f2cad104d5d0c14863622ca1805c4e6f8486c
Victor Matheus <matheuslima.o183@gmail.com>
2023-12-05 20:09:05 -0300

    new properties set in Item scriptableobject
    Color, quantity and value are useless;
    Value was replaced by Shopkeeper Buy and Sell prices

--- Assets/Scripts/ScriptableObjects/SCO_Item.cs
+++ Assets/Scripts/ScriptableObjects/SCO_Item.cs
@@ -1,13 +1,24 @@
 ﻿namespace Assets.Scripts.ScriptableObjects
 {
+    using Assets.Scripts.Core.Enums;
+    using UnityEditor.Animations;
     using UnityEngine;
 
     [CreateAssetMenu(fileName = "SCO_Item", menuName = "Assets/Item", order = 0)]
     public class SCO_Item : ScriptableObject
     {
+        public ItemType Type;
         public Sprite Sprite;
-        public Color Color;
-        public int Quantity;
-        public float Value;
+
+        [Header("Animator Controllers")]
+        public AnimatorController HeadAnimatorController;
+        public AnimatorController OutfitAnimatorController;
+
+        [Tooltip("The price that Shopkeeper sells his items")]
+        public float ShopkeeperSellPrice;
+
+        [Tooltip("The price that Shopkeeper buys items from the player")]
+        public float ShopkeeperBuyPrice;
+
     }
 }
