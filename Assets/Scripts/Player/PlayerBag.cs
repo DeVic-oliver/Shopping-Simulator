93d26f557bdcf4ceb1d4f412ede4c8aedfd3c77d
Victor Matheus <matheuslima.o183@gmail.com>
2023-12-05 20:12:00 -0300

    Player bag
    Carries the basic functionalities for player inventory.
    Populates gameobject lists to show player items

---
+++ Assets/Scripts/Player/PlayerBag.cs
@@ -0,0 +1,48 @@
+﻿namespace Assets.Scripts.Player
+{
+    using Assets.Scripts.ScriptableObjects;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [RequireComponent(typeof(PlayerItemEquipGateway))]    
+    public class PlayerBag : MonoBehaviour
+    {
+        public static int PlayerMoney { get; private set; }
+
+        [SerializeField] private int _money = 200;
+        [SerializeField] private GameObject _playerItemPrefab;
+        [SerializeField] private List<GameObject> _playerUIBagsObject;
+
+        private PlayerItemEquipGateway _playerEquipGateway;
+
+
+        public void InsertItemIntoBag(SCO_Item itemData)
+        {
+            InjectComponents(itemData);
+            PopulateUIBagsWithObjects();
+        }
+
+        private void InjectComponents(SCO_Item itemData)
+        {
+            GetPlayerItemComponent().ItemData = itemData;
+            GetPlayerItemComponent().EquipGateway = _playerEquipGateway;
+        }
+
+        private void PopulateUIBagsWithObjects()
+        {
+            foreach (GameObject uiBags in _playerUIBagsObject)
+                Instantiate(_playerItemPrefab, uiBags.transform);
+        }
+
+        private PlayerBagItem GetPlayerItemComponent()
+        {
+            return _playerItemPrefab.GetComponent<PlayerBagItem>();
+        }
+
+        private void Awake()
+        {
+            PlayerMoney = _money;
+            _playerEquipGateway = GetComponent<PlayerItemEquipGateway>();
+        }
+    }
+}
