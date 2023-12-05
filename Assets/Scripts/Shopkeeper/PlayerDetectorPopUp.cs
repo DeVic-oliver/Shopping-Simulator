9d6859a71768c33d089721d65dd1e0d7f0431ace
Victor Matheus <matheuslima.o183@gmail.com>
2023-12-04 22:57:19 -0300

    Events for interaction with shopkeeper when player detected

--- Assets/Scripts/Shopkeeper/PlayerDetectorPopUp.cs
+++ Assets/Scripts/Shopkeeper/PlayerDetectorPopUp.cs
@@ -1,27 +1,47 @@
 ﻿namespace Assets.Scripts.Shopkeeper
 {
     using UnityEngine;
-    
+    using UnityEngine.Events;
+
     public class PlayerDetectorPopUp : MonoBehaviour
     {
+        public UnityEvent OnPlayerDetected;
+        public UnityEvent OnPlayerInteraction;
+        public UnityEvent OnPlayerOutDetection;
+
+        [SerializeField] private KeyCode _interactionKey;
+
         private readonly string _playerTag = "Player";
+        private bool _isPlayerNeaby;
 
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (IsPlayerTag(collision))
-                Debug.Log("Hello! Wanna buy something!?");
+            {
+                _isPlayerNeaby = true;
+                OnPlayerDetected?.Invoke();
+            }
         }
 
         private void OnTriggerExit2D(Collider2D collision)
         {
             if (IsPlayerTag(collision))
-                Debug.Log("Bye! Come back soon!");
+            {
+                _isPlayerNeaby = false;
+                OnPlayerOutDetection?.Invoke();
+            }
         }
 
         private bool IsPlayerTag(Collider2D collision)
         {
             return collision.CompareTag(_playerTag);
+        }
+
+        private void Update()
+        {
+            if(_isPlayerNeaby && Input.GetKeyDown(_interactionKey))
+                OnPlayerInteraction?.Invoke();
         }
 
     }
