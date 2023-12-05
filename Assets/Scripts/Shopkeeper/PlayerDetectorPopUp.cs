682240cc1eddfe840fc2624d0bfec12674969bc3
Victor Matheus <matheuslima.o183@gmail.com>
2023-12-04 21:26:21 -0300

    Simple player detector for shopkeeper created

---
+++ Assets/Scripts/Shopkeeper/PlayerDetectorPopUp.cs
@@ -0,0 +1,28 @@
+﻿namespace Assets.Scripts.Shopkeeper
+{
+    using UnityEngine;
+    
+    public class PlayerDetectorPopUp : MonoBehaviour
+    {
+        private readonly string _playerTag = "Player";
+
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (IsPlayerTag(collision))
+                Debug.Log("Hello! Wanna buy something!?");
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (IsPlayerTag(collision))
+                Debug.Log("Bye! Come back soon!");
+        }
+
+        private bool IsPlayerTag(Collider2D collision)
+        {
+            return collision.CompareTag(_playerTag);
+        }
+
+    }
+}
