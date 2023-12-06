c3243b698c1adfce56e1f41ea600b83d08b3fd9d
Victor Matheus <matheuslima.o183@gmail.com>
2023-12-05 22:42:18 -0300

    Shop class
    through this class the player can buy items from shopkeeper

---
+++ Assets/Scripts/Shopkeeper/Shop.cs
@@ -0,0 +1,29 @@
+﻿namespace Assets.Scripts.Shopkeeper
+{
+    using Assets.Scripts.Player;
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class Shop : MonoBehaviour
+    {
+        public UnityEvent OnSuccessSell;
+        public UnityEvent OnFailSell;
+
+        [SerializeField] private PlayerBag _playerBag;
+
+
+        public void SellToPlayer(ShopkeeperItem item)
+        {
+            if(PlayerBag.PlayerMoney >= item.ItemData.ShopkeeperSellPrice)
+            {
+                _playerBag.DecreasePlayerMoneyByItemSellPrice(item.ItemData.ShopkeeperSellPrice);
+                _playerBag.InsertItemIntoBag(item.ItemData);
+                OnSuccessSell?.Invoke();
+            }
+            else
+            {
+                OnFailSell?.Invoke();
+            }
+        }
+    }
+}
